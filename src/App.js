6ee79eec007abf773cebc249dd125f5be96385c2
jasonngsmg <ngjingsong@streamframe.com>
2023-03-03 14:51:34 +0800

    update drawing catching logic

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ function App() {
   var canvas;
   var context;
   var isDrawing = false;
+  var drawingResult = -1;
+  var checkDrawingResult = false;
 
   window.onload = function () {
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
     canvas.width = 1000;
     canvas.height = 900;
-    context.strokeStyle = "#913d88";
+    context.strokeStyle = "blue";
     context.lineWidth = 10;
 
     context.fillStyle = "red";
     context.fillRect(0, 0, 800, 500);
     context.fillStyle = "lime";
-    context.fillRect(400, 0, 100, 500);
+    context.save();
+    context.rotate((45 * Math.PI) / 180);
+    context.fillRect(300, -300, 100, 500);
+    context.restore();
 
     canvas.onmousedown = startDrawing;
     canvas.onmouseup = stopDrawing;
@@ -30,30 +35,38 @@ function App() {
 
     function draw(e) {
       if (isDrawing === true) {
-        var x = e.pageX - canvas.offsetLeft;
-        var y = e.pageY - canvas.offsetTop;
+        let x = e.pageX - canvas.offsetLeft;
+        let y = e.pageY - canvas.offsetTop;
 
-        var rgba = context.getImageData(x, y, 1, 1).data;
+        let rgba = context.getImageData(x, y, 1, 1).data;
 
         if (rgba[0] === 255 && rgba[1] === 0 && rgba[2] === 0) {
-          console.log(false);
+          if (drawingResult === -1 || drawingResult === 1) {
+            drawingResult = 0;
+          }
         } else if (rgba[0] === 0 && rgba[1] === 255 && rgba[2] === 0) {
-          console.log(true);
+          if (drawingResult === -1 || drawingResult !== 0) {
+            drawingResult = 1;
+          }
         }
 
         context.lineTo(x, y);
         context.stroke();
-      } else {
-        var x = e.clientX;
-        var y = e.clientY;
-
-        var rgba = context.getImageData(x, y, 1, 1).data;
-        // console.log(rgba[0], rgba[1], rgba[2]);
       }
     }
 
     function stopDrawing() {
       isDrawing = false;
+
+      if (drawingResult === 1) {
+        checkDrawingResult = true;
+      } else if (drawingResult === 0) {
+        checkDrawingResult = false;
+      }
+
+      console.log(checkDrawingResult);
+
+      drawingResult = -1;
 
       // Clear drawed line
       // context.clearRect(0, 0, canvas.width, canvas.height);
