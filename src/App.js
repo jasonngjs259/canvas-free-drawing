5b661567ed6f609d29ff4c6c893dc11a8b8f662d
jasonngsmg <ngjingsong@streamframe.com>
2023-03-06 11:12:00 +0800

    update drawing feature

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   var isDrawing = false;
   var drawingResult = -1;
   var checkDrawingResult = false;
-  // var canvasStep = -1;
+  var startPointX = 0;
+  var startPointY = 0;
 
   window.onload = function () {
     canvas = document.getElementById("canvas");
@@ -20,24 +21,48 @@ function App() {
     canvas.onmouseup = stopDrawing;
     canvas.onmousemove = draw;
 
+    function drawDottedLine(fromX, fromY, toX, toY) {
+      context.lineWidth = 2;
+      context.strokeStyle = "black";
+      context.setLineDash([5, 15]);
+
+      context.moveTo(fromX, fromY);
+      context.lineTo(toX, toY);
+      context.stroke();
+    }
+
     function drawInitial() {
-      context.fillStyle = "rgba(255, 0, 0, 0)";
+      context.fillStyle = "rgba(255, 0, 0, 1)"; // red
       context.fillRect(0, 0, 800, 900);
-      context.fillStyle = "rgba(0, 255, 0, 1)";
+      context.fillStyle = "rgba(0, 255, 0, 1)"; // lime
+
+      context.beginPath();
+
       context.save();
       context.rotate((75 * Math.PI) / 180);
       context.fillRect(200, -600, 100, 500);
+      drawDottedLine(250, -600, 250, -130);
       context.restore();
+
       context.save();
       context.rotate((285 * Math.PI) / 180);
       context.fillRect(-210, 200, 100, 525);
+      drawDottedLine(-160, 250, -160, 725);
       context.restore();
+
+      // context.save();
+      // context.rotate((285 * Math.PI) / 180);
+      // context.fillRect(-210, 200, 100, 525);
+      // drawDottedLine(-160, 250, -160, 725);
+      // context.restore();
     }
 
     function startDrawing(e) {
       isDrawing = true;
       context.beginPath();
       context.moveTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+      startPointX = e.pageX - canvas.offsetLeft;
+      startPointY = e.pageY - canvas.offsetTop;
     }
 
     function draw(e) {
@@ -59,6 +84,7 @@ function App() {
 
         context.lineTo(x, y);
         context.stroke();
+        context.restore();
       }
     }
 
