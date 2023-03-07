6f50252e8f979f7f61ccbd36249c479a8c32fefa
jasonngsmg <ngjingsong@streamframe.com>
2023-03-07 15:34:59 +0800

    adding mobile interaction

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ function App() {
   var isDrawing = false;
   var drawingResult = -1;
   var checkDrawingResult = false;
-  var startPointX = 0;
-  var startPointY = 0;
-  var endPointX = 0;
-  var endPointY = 0;
-  var isMouseLeave = 0;
+  var isMobile = false;
 
   window.onload = function () {
     canvas = document.getElementById("canvas");
@@ -19,6 +15,24 @@ function App() {
     canvas.height = 100;
     context.strokeStyle = "red";
     context.lineWidth = 3;
+
+    if (
+      navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/webOS/i) ||
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/iPod/i) ||
+      navigator.userAgent.match(/BlackBerry/i) ||
+      navigator.userAgent.match(/Windows Phone/i)
+    ) {
+      isMobile = true;
+    } else {
+      isMobile = false;
+    }
+
+    canvas.ontouchstart = startDrawing;
+    canvas.ontouchend = stopDrawing;
+    canvas.ontouchmove = draw;
 
     canvas.onmousedown = startDrawing;
     canvas.onmouseup = stopDrawing;
@@ -77,84 +91,58 @@ function App() {
     }
 
     function drawInitial() {
-      // context.fillStyle = "rgba(255, 0, 0, 0.5)"; // red
-      // context.fillRect(0, 0, 80, 100);
-      // context.fillStyle = "rgba(0, 255, 0, 0.5)"; // lime
-
       context.beginPath();
 
       context.save();
       context.rotate((75 * Math.PI) / 180);
-      // context.fillRect(48, -50, 15, 55);
       drawDottedLine(55, 5, 55, -48);
       context.restore();
 
       context.save();
       context.rotate((285 * Math.PI) / 180);
-      // context.fillRect(-30, 20, 15, 55);
       drawDottedLine(-22, 20, -22, 70);
       context.restore();
 
       context.save();
       context.rotate((75 * Math.PI) / 180);
-      // context.fillRect(20, -50, 15, 50);
       drawDottedLine(28, -8, 28, -50);
       context.restore();
-
-      //---------------------------------------------------
-
-      // context.save();
-      // context.rotate((285 * Math.PI) / 180);
-      // context.fillRect(-470, 265, 100, 525);
-      // // drawDottedLine(-415, 315, -415, 770);
-      // context.restore();
-
-      // context.save();
-      // context.rotate((75 * Math.PI) / 180);
-      // context.fillRect(710, -475, 100, 525);
-      // // drawDottedLine(755, -455, 755, 50);
-      // context.restore();
     }
 
     function startDrawing(e) {
       isDrawing = true;
       context.beginPath();
-      context.moveTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
-      startPointX = e.pageX - canvas.offsetLeft;
-      startPointY = e.pageY - canvas.offsetTop;
+
+      if (isMobile) {
+        context.moveTo(
+          e.changedTouches[0].clientX - canvas.offsetLeft,
+          e.changedTouches[0].clientY - canvas.offsetTop
+        );
+      } else {
+        context.moveTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+      }
     }
 
     function draw(e) {
+      let x = null;
+      let y = null;
       if (isDrawing === true) {
-        let x = e.pageX - canvas.offsetLeft;
-        let y = e.pageY - canvas.offsetTop;
-        // let x = e.pageX - canvas.offsetLeft;
-        // let y = e.pageY - canvas.offsetTop;
-
-        // if (rgba[0] === 255 && rgba[1] === 0 && rgba[2] === 0) {
-        //   if (drawingResult === -1 || drawingResult === 1) {
-        //     drawingResult = 0;
-        //   }
-        // } else if (rgba[0] === 0 && rgba[1] === 255 && rgba[2] === 0) {
-        //   if (drawingResult === -1 || drawingResult !== 0) {
-        //     drawingResult = 1;
-        //   }
-        // }
-
-        // console.log(drawingResult);
+        console.log(isDrawing);
+        if (isMobile) {
+          x = e.changedTouches[0].clientX - canvas.offsetLeft;
+          y = e.changedTouches[0].clientY - canvas.offsetTop;
+        } else {
+          x = e.pageX - canvas.offsetLeft;
+          y = e.pageY - canvas.offsetTop;
+        }
 
         context.lineTo(x, y);
         context.stroke();
       }
     }
 
-    function stopDrawing(e) {
+    function stopDrawing() {
       isDrawing = false;
-      // endPointX = e.pageX - canvas.offsetLeft;
-      // endPointY = e.pageY - canvas.offsetTop;
-
-      // console.log(calculateRedLine());
-      // console.log(calculateWhiteDots());
 
       let totalRed = calculateRedLine();
       let totalWhite = calculateWhiteDots();
@@ -171,20 +159,6 @@ function App() {
         checkDrawingResult = false;
       }
 
-      // console.log(checkDrawingResult);
-      // document.getElementById("result").innerHTML =
-      //   "Result: " +
-      //   checkDrawingResult +
-      //   "<br />Start Point: (" +
-      //   startPointX +
-      //   ", " +
-      //   startPointY +
-      //   ")<br />End Point: (" +
-      //   endPointX +
-      //   ", " +
-      //   endPointY +
-      //   ")";
-
       document.getElementById("result").innerHTML =
         "Result: " + checkDrawingResult;
 
@@ -196,17 +170,12 @@ function App() {
     }
 
     drawInitial();
-
-    // console.log(calculateWhiteDots());
   };
 
   return (
     <>
       <div className="App">
         <canvas id="canvas" />
-        {/* <div className="canvasContainer">
-          <canvas id="canvas" />
-        </div> */}
         <div id="result" />
       </div>
     </>
