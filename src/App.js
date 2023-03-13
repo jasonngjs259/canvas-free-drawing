b1054c13cad3b3841874d05e2b4f0dafd590fc22
jasonngsmg <ngjingsong@streamframe.com>
2023-03-13 16:53:28 +0800

    update canvas dotted line location

--- src/App.js
+++ src/App.js
@@ -7,15 +7,245 @@ function App() {
   var drawingResult = -1;
   var checkDrawingResult = false;
   var isMobile = false;
+  var dottedLine = 0;
+  var zigzag = [
+    [
+      {
+        rotation: 15,
+        coordinate: {
+          fromX: -25,
+          fromY: -15,
+          toX: 25,
+          toY: -15,
+        },
+      },
+      {
+        rotation: 345,
+        coordinate: {
+          fromX: -25,
+          fromY: 0,
+          toX: 25,
+          toY: 0,
+        },
+      },
+      {
+        rotation: 15,
+        coordinate: {
+          fromX: -25,
+          fromY: 15,
+          toX: 25,
+          toY: 15,
+        },
+      },
+    ],
+    [
+      {
+        rotation: 15,
+        coordinate: {
+          fromX: -35,
+          fromY: -30,
+          toX: 15,
+          toY: -30,
+        },
+      },
+      {
+        rotation: 345,
+        coordinate: {
+          fromX: -25,
+          fromY: -15,
+          toX: 25,
+          toY: -15,
+        },
+      },
+      {
+        rotation: 15,
+        coordinate: {
+          fromX: -25,
+          fromY: 0,
+          toX: 25,
+          toY: 0,
+        },
+      },
+      {
+        rotation: 345,
+        coordinate: {
+          fromX: -25,
+          fromY: 15,
+          toX: 25,
+          toY: 15,
+        },
+      },
+      {
+        rotation: 15,
+        coordinate: {
+          fromX: -18,
+          fromY: 30,
+          toX: 32,
+          toY: 30,
+        },
+      },
+    ],
+    [
+      {
+        rotation: 80,
+        coordinate: {
+          fromX: 60,
+          fromY: 0,
+          toX: 60,
+          toY: -58,
+        },
+      },
+      {
+        rotation: 285,
+        coordinate: {
+          fromX: -28,
+          fromY: 15,
+          toX: -28,
+          toY: 78,
+        },
+      },
+      {
+        rotation: 85,
+        coordinate: {
+          fromX: 30,
+          fromY: -6,
+          toX: 30,
+          toY: -65,
+        },
+      },
+      {
+        rotation: 285,
+        coordinate: {
+          fromX: -7,
+          fromY: 15,
+          toX: -7,
+          toY: 70,
+        },
+      },
+    ],
+  ];
+
+  var BIG_ZIGZAG = [
+    [
+      {
+        rotation: 345,
+
+        coordinate: {
+          fromX: -77,
+          fromY: -65,
+          toX: 100,
+          toY: -65,
+        },
+      },
+      {
+        rotation: 25,
+
+        coordinate: {
+          fromX: -100,
+          fromY: 0,
+          toX: 100,
+          toY: 0,
+        },
+      },
+      {
+        rotation: 345,
+
+        coordinate: {
+          fromX: -100,
+          fromY: 65,
+          toX: 100 - (100 - 77),
+          toY: 65,
+        },
+      },
+    ],
+    [
+      {
+        rotation: 15,
+        coordinate: {
+          fromX: -100,
+          fromY: -100,
+          toX: 77,
+          toY: -100,
+        },
+      },
+      {
+        rotation: 345,
+        coordinate: {
+          fromX: -82,
+          fromY: -50,
+          toX: 118,
+          toY: -50,
+        },
+      },
+      {
+        rotation: 15,
+        coordinate: {
+          fromX: -100,
+          fromY: 0,
+          toX: 100,
+          toY: 0,
+        },
+      },
+      {
+        rotation: 345,
+        coordinate: {
+          fromX: -110,
+          fromY: 50,
+          toX: 90,
+          toY: 50,
+        },
+      },
+      {
+        rotation: 15,
+        coordinate: {
+          fromX: -75,
+          fromY: 100,
+          toX: 120,
+          toY: 100,
+        },
+      },
+    ],
+    [
+      {
+        rotation: 15,
+        coordinate: {
+          fromX: -110,
+          fromY: -75,
+          toX: 68,
+          toY: -75,
+        },
+      },
+      {
+        rotation: 345,
+        coordinate: {
+          fromX: -100,
+          fromY: -30,
+          toX: 100,
+          toY: -30,
+        },
+      },
+      {
+        rotation: 15,
+        coordinate: {
+          fromX: -100,
+          fromY: 30,
+          toX: 100,
+          toY: 30,
+        },
+      },
+      {
+        rotation: 345,
+        coordinate: {
+          fromX: -125,
+          fromY: 75,
+          toX: 72,
+          toY: 75,
+        },
+      },
+    ],
+  ];
 
   window.onload = function () {
-    canvas = document.getElementById("canvas");
-    context = canvas.getContext("2d", { willReadFrequently: true });
-    canvas.width = 80;
-    canvas.height = 100;
-    context.strokeStyle = "red";
-    context.lineWidth = 3;
-
     if (
       navigator.userAgent.match(/Android/i) ||
       navigator.userAgent.match(/webOS/i) ||
@@ -30,6 +260,13 @@ function App() {
       isMobile = false;
     }
 
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d", { willReadFrequently: true });
+    canvas.width = isMobile ? 686 : 80;
+    canvas.height = isMobile ? 726 : 100;
+    context.strokeStyle = "red";
+    context.lineWidth = isMobile ? 8 : 4;
+
     canvas.ontouchstart = startDrawing;
     canvas.ontouchend = stopDrawing;
     canvas.ontouchmove = draw;
@@ -38,6 +275,36 @@ function App() {
     canvas.onmouseup = stopDrawing;
     canvas.onmousemove = draw;
     canvas.onmouseleave = stopDrawing2;
+
+    // start sample
+    // context.beginPath();
+    // context.moveTo(100, 10);
+    // context.lineTo(210, 10);
+    // context.strokeStyle = "lime";
+    // context.stroke();
+
+    // context.beginPath();
+    // context.moveTo(100, 30);
+    // context.lineTo(210, 30);
+    // context.strokeStyle = "blue";
+    // context.stroke();
+
+    // context.beginPath();
+    // context.moveTo(100, 50);
+    // context.lineTo(210, 50);
+    // context.strokeStyle = "red";
+    // context.stroke();
+
+    // context.strokeStyle = "red";
+    // context.strokeRect(310, 70, 100, 200);
+
+    // context.save();
+    // context.strokeStyle = "lime";
+    // context.strokeRect(510, 70, 100, 200);
+    // context.restore();
+
+    // context.strokeRect(710, 70, 100, 200);
+    // end sample
 
     function stopDrawing2() {
       if (isDrawing) {
@@ -93,20 +360,59 @@ function App() {
     function drawInitial() {
       context.beginPath();
 
-      context.save();
-      context.rotate((75 * Math.PI) / 180);
-      drawDottedLine(55, 5, 55, -48);
-      context.restore();
+      let randomZigzag = 1;
+      let scale = 1;
+      let selectedZigzag = isMobile
+        ? BIG_ZIGZAG[randomZigzag]
+        : zigzag[randomZigzag];
 
-      context.save();
-      context.rotate((285 * Math.PI) / 180);
-      drawDottedLine(-22, 20, -22, 70);
-      context.restore();
+      selectedZigzag.forEach((element) => {
+        // can add the switch to utils
+        switch (randomZigzag) {
+          case 0:
+            scale = isMobile ? 2.5 : 1;
+            break;
+          case 1:
+            scale = isMobile ? 1.5 : 1;
+            break;
+          case 2:
+            scale = isMobile ? 1 : 1;
+            break;
+          default:
+            scale = 1;
+            break;
+        }
 
-      context.save();
-      context.rotate((75 * Math.PI) / 180);
-      drawDottedLine(28, -8, 28, -50);
-      context.restore();
+        context.save();
+        context.translate(canvas.width / 2, canvas.height / 2);
+        context.rotate((element.rotation * Math.PI) / 180);
+        context.scale(scale, scale);
+        context.translate(-canvas.width / 2, -canvas.height / 2);
+        drawDottedLine(
+          canvas.width / 2 + element.coordinate.fromX,
+          canvas.height / 2 + element.coordinate.fromY,
+          canvas.width / 2 + element.coordinate.toX,
+          canvas.height / 2 + element.coordinate.toY
+        );
+        context.restore();
+      });
+
+      // context.save();
+      // context.rotate((75 * Math.PI) / 180);
+      // drawDottedLine(385, -276, 385, 14);
+      // context.restore();
+
+      // context.save();
+      // context.rotate((285 * Math.PI) / 180);
+      // drawDottedLine(-192, 130, -192, 430);
+      // context.restore();
+
+      // context.save();
+      // context.rotate((75 * Math.PI) / 180);
+      // drawDottedLine(228, -308, 228, -8);
+      // context.restore();
+
+      dottedLine = calculateWhiteDots();
     }
 
     function startDrawing(e) {
@@ -127,7 +433,6 @@ function App() {
       let x = null;
       let y = null;
       if (isDrawing === true) {
-        console.log(isDrawing);
         if (isMobile) {
           x = e.changedTouches[0].clientX - canvas.offsetLeft;
           y = e.changedTouches[0].clientY - canvas.offsetTop;
@@ -147,7 +452,11 @@ function App() {
       let totalRed = calculateRedLine();
       let totalWhite = calculateWhiteDots();
 
-      if (totalRed < 800 && totalRed > 500 && totalWhite < (455 * 40) / 100) {
+      if (
+        totalRed < dottedLine * 2 &&
+        totalRed > (dottedLine * 2 * 60) / 100 &&
+        totalWhite < (dottedLine * 60) / 100
+      ) {
         checkDrawingResult = true;
       } else {
         checkDrawingResult = false;
@@ -163,6 +472,8 @@ function App() {
         "Result: " + checkDrawingResult;
 
       drawingResult = -1;
+      console.log("Dotted line: " + calculateWhiteDots());
+      console.log("Red line: " + calculateRedLine());
 
       // Clear drawed line
       context.clearRect(0, 0, canvas.width, canvas.height);
